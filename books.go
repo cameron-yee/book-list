86cd22f48920ce1693735833923ba1343ea6a837
cameron-yee <yee.camero23@gmail.com>
2020-04-13 15:35:17 -0600

    Add update and delete book options

--- books.go
+++ books.go
@@ -4,6 +4,7 @@ import (
     "encoding/json"
     "fmt"
     "io/ioutil"
+    "strconv"
     "strings"
 )
 
@@ -60,12 +61,7 @@ func listBooks() {
     }
 }
 
-func writeBook(book *Book) {
-    var books Books
-    books = readBooks()
-    
-    books.Books = append(books.Books, *book)
-
+func writeBooks(books Books) {
     dataBytes, err := json.Marshal(books)
     if err != nil {
         panic(err)
@@ -75,6 +71,15 @@ func writeBook(book *Book) {
     if err != nil {
         panic(err)
     }
+}
+
+func appendBook(book *Book) {
+    var books Books
+    books = readBooks()
+    
+    books.Books = append(books.Books, *book)
+
+    writeBooks(books)
 }
 
 func addBook() {
@@ -107,7 +112,158 @@ func addBook() {
         Genre: genre,
     }
 
-    writeBook(new_book)
+    appendBook(new_book)
 }
 
+func runUpdate(book_title, field, value string) {
+    switch strings.ToLower(field) {
+        case "title":
+            updateBookTitle(book_title, value)
+        case "series":
+            updateBookSeries(book_title, value)
+        case "author":
+            updateBookAuthor(book_title, value)
+        case "recommendedby":
+            updateBookRecommendedBy(book_title, value)
+        case "read":
+            value_as_bool, _ := strconv.ParseBool(value)
+            updateBookRead(book_title, value_as_bool)
+        case "owned":
+            value_as_bool, _ := strconv.ParseBool(value)
+            updateBookOwned(book_title, value_as_bool)
+        case "genre":
+            updateBookGenre(book_title, value)
+        default:
+            fmt.Print("Option not set up.")
+    }
+}  
 
+func updateBookTitle(book_title, title_value string) {
+    var books Books
+    books = readBooks()
+    
+    var my_books []Book = books.Books
+
+    for i := 0; i < len(my_books); i++ {
+        if strings.ToLower(my_books[i].Title) == strings.ToLower(book_title) {
+            my_books[i].Title = title_value
+            break
+        }
+    }
+    
+    writeBooks(books)
+}
+
+func updateBookSeries(book_title, series_value string) {
+    var books Books
+    books = readBooks()
+    
+    var my_books []Book = books.Books
+
+    for i := 0; i < len(my_books); i++ {
+        if strings.ToLower(my_books[i].Title) == strings.ToLower(book_title) {
+            my_books[i].Series = series_value
+            break
+        }
+    }
+    
+    writeBooks(books)
+}
+
+func updateBookAuthor(book_title, author_value string) {
+    var books Books
+    books = readBooks()
+    
+    var my_books []Book = books.Books
+
+    for i := 0; i < len(my_books); i++ {
+        if strings.ToLower(my_books[i].Title) == strings.ToLower(book_title) {
+            my_books[i].Author = author_value
+            break
+        }
+    }
+    
+    writeBooks(books)
+}
+
+func updateBookRecommendedBy(book_title, recommended_by_value string) {
+    var books Books
+    books = readBooks()
+    
+    var my_books []Book = books.Books
+
+    for i := 0; i < len(my_books); i++ {
+        if strings.ToLower(my_books[i].Title) == strings.ToLower(book_title) {
+            my_books[i].RecommendedBy = recommended_by_value
+            break
+        }
+    }
+    
+    writeBooks(books)
+}
+
+func updateBookRead(book_title string, read_value bool) {
+    var books Books
+    books = readBooks()
+    
+    var my_books []Book = books.Books
+
+    for i := 0; i < len(my_books); i++ {
+        if strings.ToLower(my_books[i].Title) == strings.ToLower(book_title) {
+            my_books[i].Read = read_value
+            break
+        }
+    }
+    
+    writeBooks(books)
+}
+
+func updateBookOwned(book_title string, owned_value bool) {
+    var books Books
+    books = readBooks()
+    
+    var my_books []Book = books.Books
+
+    for i := 0; i < len(my_books); i++ {
+        if strings.ToLower(my_books[i].Title) == strings.ToLower(book_title) {
+            my_books[i].Owned = owned_value
+            break
+        }
+    }
+    
+    writeBooks(books)
+}
+
+func updateBookGenre(book_title, genre_value string) {
+    var books Books
+    books = readBooks()
+    
+    var my_books []Book = books.Books
+
+    for i := 0; i < len(my_books); i++ {
+        if strings.ToLower(my_books[i].Title) == strings.ToLower(book_title) {
+            my_books[i].Genre = genre_value
+            break
+        }
+    }
+    
+    writeBooks(books)
+}
+
+func deleteBook(book_title string) {
+    var books Books
+    books = readBooks()
+    
+    var my_books []Book = books.Books
+
+    i := 0
+    for ; i < len(my_books); i++ {
+        if strings.ToLower(my_books[i].Title) == strings.ToLower(book_title) {
+            break
+        }
+    }
+
+    books.Books = append(books.Books[:i], books.Books[i+1:]...)
+    
+    writeBooks(books)
+}
--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 
 func help() {
     fmt.Println("Commands:")
+    fmt.Println("\tadd: Add a book.")
+    fmt.Println("\tdelete: Delete a book by supplying the book title.")
+    fmt.Println("\tfilter: Filter based on bool property. Ex. ./main.go filter owned true")
     fmt.Println("\thelp: Print list of commands.")
     fmt.Println("\tlist: List books.")
-    fmt.Println("\tadd: Add a book.")
-    fmt.Println("\tfilter: Filter based on bool property. Ex. ./main.go filter owned true")
     fmt.Println("\tsearch: Search based on string property. Ex. ./main.go search title Narnia")
+    fmt.Println("\tupdate: Update a book by providing a book title, a field, and the new value for the field.")
 }
 
 func main() {
@@ -22,16 +24,14 @@ func main() {
     }
 
     switch command := os.Args[1]; command {
-        case "list":
-            listBooks()
         case "add":
             addBook()
-        case "search":
-            if len(os.Args) != 4 {
-                panic("Please provide a search and value.")
+        case "delete":
+            if len(os.Args) != 3 {
+                panic("Please provide a book title to delete.")
             } else {
-                runSearch(os.Args[2], os.Args[3])
-            }
+                deleteBook(os.Args[2])
+            } 
         case "filter":
             if len(os.Args) != 4 {
                 panic("Please provide a filter and value.")
@@ -42,7 +42,21 @@ func main() {
                 }
                 
                 runFilter(os.Args[2], value)
-            } 
+            }
+        case "list":
+            listBooks()
+        case "search":
+            if len(os.Args) != 4 {
+                panic("Please provide a search and value.")
+            } else {
+                runSearch(os.Args[2], os.Args[3])
+            }
+        case "update":
+            if len(os.Args) != 5 {
+                panic("Please provide a title, a field to update, and the new value for the field.")
+            } else {
+                runUpdate(os.Args[2], os.Args[3], os.Args[4]) // title, field, field_value
+            }
         default:
             help()
     }
